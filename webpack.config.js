aa4ea93ccb817a05e2eb1064b6ed8f4902cb47cb
iabdul79 <mabdulwadood79@gmail.com>
2022-10-04 22:47:24 +0530

    chors: web-pack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,60 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const outputPath = path.join(__dirname, 'public')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: outputPath,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
-            ]
+module.exports = (env) => {
+  const isProduction = env.production
+  return {
+    entry: './src/app.js',
+    output: {
+      path: outputPath,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react"
+              ]
+            }
           }
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
         }
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    contentBase: outputPath,
-    historyApiFallback: true
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ 
+        filename: 'styles.css'
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-cheap-module-source-map',
+    devServer: {
+      contentBase: outputPath,
+      historyApiFallback: true
+    }
   }
 }
